7fa2a03de139fa6dcafdc9c8f52e7518c02773c9
Daniel Brazel <dbrazel4@gmail.com>
2018-03-08 18:16:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown(int)
   end
   "Happy New Year!".upcase
 end
+
+def countdown_with_sleep
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+  "Happy New Year!".upcase
+end
